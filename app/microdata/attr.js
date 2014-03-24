49185c4addeca673cdc45bfffabc04f354336257
Estelle DeBlois <edeblois@gmail.com>
2014-03-24 15:58:50 -0700

    handle changes to the model

--- app/microdata/attr.js
+++ app/microdata/attr.js
@@ -1,5 +1,12 @@
 export default function() {
-  return function(key) {
-    return this.$data[key];
-  }.property()
+  return function(key, value) {
+    if (arguments.length === 1) {
+      return this.$changes[key] || this.$data[key];
+    } else {
+      var changes = this.$changes;
+      changes[key] = value;
+      this.set('$changes', changes);
+      return value;
+    }
+  }.property('$data', '$changes')
 }
--- app/microdata/model.js
+++ app/microdata/model.js
@@ -1,1 +1,11 @@
-export default Ember.Object.extend();
+export default Ember.Object.extend({
+
+  init: function() {
+    this.set('$changes', {});
+  },
+
+  $mergeChanges: function() {
+    $.extend(this.get('$data'), this.get('$changes'));
+    this.set('$changes', {});
+  }
+});
